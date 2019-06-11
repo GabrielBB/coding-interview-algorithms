5e7544dc2c71c8fcb2c8a64d51540539f9151d6a
Gabriel Basilio Brito <gabrielbb0306@gmail.com>
2019-06-11 14:18:23 -0400

    Committing Polygons

--- src/main/java/com/github/gabrielbb/bhacking/Polygons.java
+++ src/main/java/com/github/gabrielbb/bhacking/Polygons.java
@@ -1,5 +1,8 @@
 package com.github.gabrielbb.bhacking;
 
+import java.util.*;
+import java.util.stream.Stream;
+
 /**
  * Hello world!
  */
@@ -10,6 +13,34 @@ public final class Polygons {
      * @param args The arguments of the program.
      */
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        
+        try(Scanner scanner = new Scanner(System.in)) {
+            int squares = 0;
+            int rectangles = 0;
+            int other = 0;
+
+            int lines = scanner.nextInt();
+            scanner.nextLine();
+
+            while(lines > 0) {
+                String line = scanner.nextLine().trim();
+    
+                if(!line.isEmpty()) {
+                    int[] values = Stream.of(line.trim().split(" ")).mapToInt(Integer::parseInt).toArray();
+    
+                    if(values[0] == values[1] && values[1] == values[2] && values[2] == values[3]) {
+                        squares++;
+                    } else if (values[0] == values[2] && values[1] == values[3]) {
+                        rectangles++;
+                    } else {
+                        other++;
+                    }
+                }
+
+                lines--;
+            }
+    
+            System.out.println(squares + " " + rectangles + " " + other);
+        };
     }
 }
