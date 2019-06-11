6520aa555a3eb53e8992386cf3769966dae7a002
Gabriel Basilio Brito <gabrielbb0306@gmail.com>
2019-06-11 14:20:24 -0400

    Reducir Polygons code

--- src/main/java/com/github/gabrielbb/bhacking/Polygons.java
+++ src/main/java/com/github/gabrielbb/bhacking/Polygons.java
@@ -10,11 +10,12 @@ public final class Polygons {
 
     /**
      * Says hello to the world.
+     * 
      * @param args The arguments of the program.
      */
     public static void main(String[] args) {
-        
-        try(Scanner scanner = new Scanner(System.in)) {
+
+        try (Scanner scanner = new Scanner(System.in)) {
             int squares = 0;
             int rectangles = 0;
             int other = 0;
@@ -22,25 +23,21 @@ public final class Polygons {
             int lines = scanner.nextInt();
             scanner.nextLine();
 
-            while(lines > 0) {
-                String line = scanner.nextLine().trim();
-    
-                if(!line.isEmpty()) {
-                    int[] values = Stream.of(line.trim().split(" ")).mapToInt(Integer::parseInt).toArray();
-    
-                    if(values[0] == values[1] && values[1] == values[2] && values[2] == values[3]) {
-                        squares++;
-                    } else if (values[0] == values[2] && values[1] == values[3]) {
-                        rectangles++;
-                    } else {
-                        other++;
-                    }
+            while (lines > 0) {
+                int[] values = Stream.of(scanner.nextLine().trim().split(" ")).mapToInt(Integer::parseInt).toArray();
+
+                if (values[0] == values[1] && values[1] == values[2] && values[2] == values[3]) {
+                    squares++;
+                } else if (values[0] == values[2] && values[1] == values[3]) {
+                    rectangles++;
+                } else {
+                    other++;
                 }
 
                 lines--;
             }
-    
+
             System.out.println(squares + " " + rectangles + " " + other);
-        };
+        }
     }
 }
