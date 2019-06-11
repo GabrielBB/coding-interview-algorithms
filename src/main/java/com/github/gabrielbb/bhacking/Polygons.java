0ae271598ff4acc55258fe51b8b9a601d306f904
Gabriel Basilio Brito <gabrielbb0306@gmail.com>
2019-06-11 13:17:02 -0400

    Initial Maven structure commit

---
+++ src/main/java/com/github/gabrielbb/bhacking/Polygons.java
@@ -0,0 +1,15 @@
+package com.github.gabrielbb.bhacking;
+
+/**
+ * Hello world!
+ */
+public final class Polygons {
+
+    /**
+     * Says hello to the world.
+     * @param args The arguments of the program.
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/test/java/com/github/gabrielbb/bhacking/Tests.java
@@ -0,0 +1,18 @@
+package com.github.gabrielbb.bhacking;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Unit test for simple App.
+ */
+public class Tests {
+    /**
+     * Rigorous Test.
+     */
+    @Test
+    public void testApp() {
+        assertTrue(true);
+    }
+}
