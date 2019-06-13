23bdc6ece043c00206e16f24a5902739404bff1b
Gabriel Basilio Brito <gabrielbb0306@gmail.com>
2019-06-12 22:11:47 -0400

    Initial Commit for MergeSort

--- src/main/java/com/github/gabrielbb/bhacking/MergeSort.java
+++ src/main/java/com/github/gabrielbb/bhacking/MergeSort.java
@@ -1,8 +1,21 @@
 package com.github.gabrielbb.bhacking;
 
+import java.util.Scanner;
+import java.util.stream.Stream;
+
 public class MergeSort {
 
     public static void main(String[] args) {
 
+        try(Scanner scanner = new Scanner(System.in)) {
+            int[] array = Stream.of(scanner.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+            mergeSort(array, 0, array.length - 1);
+         }
+    }
+
+    private static void mergeSort(int[] array, int left, int right) {
+        int middle = (left + right) / 2;
+        mergeSort(array, left, middle);
+        mergeSort(array, middle, right);
     }
 }
