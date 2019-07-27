91065ec78d8c5f2581a3943bf677f6ccafdb93d1
Gabriel Basilio Brito <gabrielbb0306@gmail.com>
2019-07-27 15:21:15 -0400

    Comitting LongestNoRepeatedCharSubtring

---
+++ src/main/java/com/github/gabrielbb/bhacking/LongestNoRepeatedCharSubstring.java
@@ -0,0 +1,37 @@
+package com.github.gabrielbb.bhacking;
+
+public class LongestNoRepeatedCharSubstring {
+
+    public static int find(String s) {
+
+        int max = 0;
+        Integer[] chars = new Integer[256];
+
+        chars[s.charAt(0)] = 0;
+        int currentCount = 1;
+
+        for (int i = 1; i < s.length(); i++) {
+            int c = s.charAt(i);
+
+            if (chars[c] != null && chars[c] >= i - currentCount) {
+                if (currentCount > max) {
+                    max = currentCount;
+                }
+
+                currentCount = i - (chars[c] + 1);
+            } else {
+                currentCount++;
+            }
+
+            chars[c] = i;
+        }
+
+        return max;
+    }
+
+    public static void main(String args[]) {
+        System.out.println(find("GEEKSFORGEEKS"));
+        System.out.println(find("ABDEFGABEF"));
+        System.out.println(find("BBBB"));
+    }
+}
